d98e5459cf6be6dbc39f1434e31b2f4d9c8a5592
xiaoxiao <15185337922>
2022-05-30 16:03:10 +0800

    third_commit

--- src/main/java/com/itcast/a_GitTest.java
+++ src/main/java/com/itcast/a_GitTest.java
@@ -4,5 +4,6 @@ public class a_GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
