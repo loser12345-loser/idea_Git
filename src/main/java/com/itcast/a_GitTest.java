35f289516081ebf96f3cbc0dcce4479d7412a2c0
xiaoxiao <15185337922>
2022-05-31 18:40:53 +0800

    push10_commit

--- src/main/java/com/itcast/a_GitTest.java
+++ src/main/java/com/itcast/a_GitTest.java
@@ -2,15 +2,12 @@ package com.itcast;
 
 public class a_GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("master_test");
         System.out.println("hot_fix test");
-        System.out.println("aa");
-        System.out.println("bb");
-        System.out.println("cc");
-        System.out.println("dd");
+
     }
 }
